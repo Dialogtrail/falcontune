379393b9c51a05640983f8f483900b2b0014d78f
mightynerd <staszw@gmail.com>
2023-06-27 13:28:53 +0200

    Add saving of full checkpoint

--- falcontune/finetune.py
+++ falcontune/finetune.py
@@ -1,4 +1,5 @@
 import os
+from os.path import exists, join, isdir
 import torch
 
 import wandb
@@ -11,7 +12,36 @@ from falcontune.model import load_model
 from falcontune.model.lora import load_adapter
 from falcontune.model.utils import model_to_half
 
+from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
+
 logger = logging.get_logger("transformers")
+
+class SavePeftModelCallback(transformers.TrainerCallback):
+    def save_model(self, args, state, kwargs):
+        print('Saving PEFT checkpoint...')
+        if state.best_model_checkpoint is not None:
+            checkpoint_folder = os.path.join(state.best_model_checkpoint, "adapter_model")
+        else:
+            checkpoint_folder = os.path.join(args.output_dir, f"{PREFIX_CHECKPOINT_DIR}-{state.global_step}")
+
+        peft_model_path = os.path.join(checkpoint_folder, "adapter_model")
+        kwargs["model"].save_pretrained(peft_model_path)
+
+        pytorch_model_path = os.path.join(checkpoint_folder, "pytorch_model.bin")
+        if os.path.exists(pytorch_model_path):
+            os.remove(pytorch_model_path)
+
+    def on_save(self, args, state, control, **kwargs):
+        self.save_model(args, state, kwargs)
+        return control
+
+    def on_train_end(self, args, state, control, **kwargs):
+        def touch(fname, times=None):
+            with open(fname, 'a'):
+                os.utime(fname, times)
+
+        touch(join(args.output_dir, 'completed'))
+        self.save_model(args, state, kwargs)
 
 
 class FinetuneConfig:
@@ -139,6 +169,7 @@ def finetune(args):
             args=training_arguments,
             data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
         )
+        trainer.add_callback(SavePeftModelCallback)
         model.config.use_cache = False
 
         # Set Model dict
